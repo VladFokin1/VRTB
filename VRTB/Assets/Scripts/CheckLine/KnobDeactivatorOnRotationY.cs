eb7b3bfe8587a60f6fbd191bf4c53e7f6e07ebb4
Andrey Murilev <andr-evklidm@mail.ru>
2025-04-16 01:12:17 +0300

    update

---
+++ VRTB/Assets/Scripts/CheckLine/KnobDeactivatorOnRotationY.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class KnobDeactivatorOnRotationY : MonoBehaviour
+{
+    [Header("Целевой угол по Y")]
+    public float targetAngleY = -40f;
+
+    [Tooltip("Допустимая погрешность")]
+    public float tolerance = 5f;
+
+    [Header("XR Knob (скрипт, который нужно отключить)")]
+    public MonoBehaviour xrKnobScript; // можно использовать XRKnob, если ты знаешь точный тип
+
+    [Header("Toggle, который включается")]
+    public Toggle targetToggle;
+
+    private bool hasTriggered = false;
+
+    void Update()
+    {
+        if (hasTriggered) return;
+
+        float currentY = NormalizeAngle(transform.localEulerAngles.y);
+
+        if (Mathf.Abs(currentY - targetAngleY) <= tolerance)
+        {
+            if (xrKnobScript != null)
+            {
+                xrKnobScript.enabled = false;
+                Debug.Log("XR Knob отключён");
+            }
+
+            if (targetToggle != null)
+            {
+                targetToggle.isOn = true;
+                Debug.Log("Toggle включён");
+            }
+
+            hasTriggered = true;
+        }
+    }
+
+    // Приводит угол к диапазону [-180, 180]
+    float NormalizeAngle(float angle)
+    {
+        angle %= 360f;
+        if (angle > 180f) angle -= 360f;
+        return angle;
+    }
+}
