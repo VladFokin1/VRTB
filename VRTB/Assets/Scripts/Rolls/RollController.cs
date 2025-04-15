793543262eab7487cf8e3ffdb25c122c738d02b2
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2025-04-16 02:01:44 +0300

    some update

--- VRTB/Assets/Scripts/Rolls/RollController.cs
+++ VRTB/Assets/Scripts/Rolls/RollController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class RollController : MonoBehaviour
 {
@@ -6,12 +8,41 @@ public class RollController : MonoBehaviour
     [SerializeField] private float _rotationSpeed = 5f;
     [SerializeField] private Axis _rotateAround;
     private AudioSource _audio;
+    [SerializeField] private XRSocketInteractor _socket1;
+    [SerializeField] private XRSocketInteractor _socket2;
+    [SerializeField] private TaskManager _taskManager;
 
     //[SerializeField] private 
 
     private void Awake()
     {
         _audio = GetComponent<AudioSource>();
+        if (!gameObject.name.Contains("BigRoll"))
+        {
+            _socket1.selectEntered.AddListener(FlagPlaced);
+            _socket1.selectExited.AddListener(FlagRemoved);
+            _socket2.selectEntered.AddListener(FlagPlaced);
+            _socket2.selectExited.AddListener(FlagRemoved);
+        }
+        
+    }
+
+    private void FlagPlaced(SelectEnterEventArgs args)
+    {
+        // args.interactableObject - это объект, который вставили в сокет
+        GameObject placedObject = args.interactableObject.transform.gameObject;
+        Debug.Log($"В сокет вставлен объект: {placedObject.name}");
+        if (_isBroken)
+        {
+            _taskManager.MarkTask(2, true);
+        }
+    }
+    private void FlagRemoved(SelectExitEventArgs args)
+    {
+        if (_isBroken)
+        {
+            _taskManager.MarkTask(2, false);
+        }
     }
 
     public void Break()
